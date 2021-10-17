59a20005d37bfd8bdfadd8a01c4cdd5df7210562
vishal kumar <vishalchoubey2@gmzil.com>
2021-10-17 14:58:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./components/cards";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Set initial state
+    this.state = { users_data: [], loading: false };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2000
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitems">
+            <h2>LetsGrowMore</h2>
+            
+            <button className="fetchbtn" onClick={this.updateState}>
+              Get Users
+            </button>
+          </div>
+        </nav>
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+        <div class="container my-50">
+          
+        <footer className="footer">
+         Lets Grow More Task-2 by Vishal Kumar 
+        
+        </footer>
+        <footer className="footer">
+          Web Development Internship
+        
+        </footer>
+        <footer className="footer">
+          2021-2022
+
+        </footer>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
